77fa8dc4eaf55e379083283ec10516895e2f2f02
Antonyan7 <you@example.com>
2018-09-29 00:32:09 +0400

    GraphQL api

--- handler.js
+++ handler.js
@@ -1,14 +1,48 @@
 'use strict';
 
-module.exports.hello = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+const {
+  graphql,
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLNonNull
+} = require('graphql')
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+const getGreeting = firstName => `Hello, ${firstName}.`
+
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'RootQueryType', // an arbitrary name
+    fields: {
+      // the query has a field called 'greeting'
+      greeting: {
+        // we need to know the user's name to greet them
+        args: { firstName: { name: 'firstName', type: new GraphQLNonNull(GraphQLString) } },
+        // the greeting message is a string
+        type: GraphQLString,
+        // resolve to a greeting message
+        resolve: (parent, args) => getGreeting(args.firstName)
+      }
+    }
+  }),
+})
+
+module.exports.query = (event, context, callback) => graphql(schema, event.queryStringParameters.query)
+  .then(
+    result => callback(null, {statusCode: 200, body: JSON.stringify(result)}),
+    err => callback(err)
+  )
+
+
+// module.exports.hello = async (event, context) => {
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify({
+//       message: 'Go Serverless v1.0! Your function executed successfully!',
+//       input: event,
+//     }),
+//   };
+//
+//   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+//   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+// };
